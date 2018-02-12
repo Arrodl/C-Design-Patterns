c4845b9eb300604d742c0f147d1f735bd4ef9b00
Arrodl <arrodl40@gmail.com>
2018-02-11 18:29:55 -0600

    jej

---
+++ DesignPatterns/DesignPatterns/Builder/HTMLBuilder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Builder {
+    class HTMLBuilder {
+        HTMLElement _root = new HTMLElement();
+        private readonly string _rootName;
+
+        public HTMLBuilder (string rootName) {
+            _root._name = rootName;
+            this._rootName = rootName;
+        }
+
+        public void AddChild(string childName, string childText) {
+            var e = new HTMLElement(childName, childText);
+            _root._elements.Add(e);
+        }
+        
+        public override string ToString() {
+            return _root.ToString();
+        }
+
+        public void Clear() {
+            _root = new HTMLElement { _name = _rootName };
+        }
+    }
+}
